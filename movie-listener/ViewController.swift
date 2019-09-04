ebc84b8d5136677330525f083698ce6ac203131d
Kamaal Farah <kamaal.f1@gmail.com>
2019-09-04 22:33:31 +0200

    Upon switching the language, the theme changes

--- movie-listener/ViewController.swift
+++ movie-listener/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     @IBOutlet weak var languageSegment: UISegmentedControl!
     
+    @IBOutlet weak var scroller: UIScrollView!
+    @IBOutlet weak var innerView: UIView!
     @IBOutlet var MovieList: [UILabel]!
     @IBOutlet var yearList: [UILabel]!
     
@@ -31,6 +33,28 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     var model = [MovieMap]()
     var recognitionTask: SFSpeechRecognitionTask?
     var isRecording = false
+    
+    enum Theme {
+        case
+        start,
+        startColor,
+        stop,
+        stopColor,
+        backgroundColor,
+        spokenTextColor,
+        resultsColor
+    }
+    
+    var theme: [Theme: Any] = [
+        Theme.start: "START",
+        Theme.startColor: UIColor.gray,
+        Theme.stop: "STOP",
+        Theme.stopColor : UIColor.red,
+        Theme.backgroundColor: UIColor.white,
+        Theme.spokenTextColor: UIColor.black,
+        Theme.resultsColor: #colorLiteral(red: 0.4573462605, green: 0.04310884327, blue: 0, alpha: 1)
+    ]
+    
     var outputLanguage = "en-AU"
     
     func requestSpeechAutherization() -> Void {
@@ -61,6 +85,7 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
         super.viewDidLoad()
         
         self.spokenTextLabel.text = ""
+        self.tapToSpeakButton.setTitleColor(self.theme[Theme.startColor] as? UIColor, for: .normal)
         self.titleLengthStepperValue.autorepeat = true
         self.titleLengthStepperValue.minimumValue = 1.0
         self.titleLengthStepperValue.maximumValue = 4.0
@@ -69,6 +94,7 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             self.yearList[index].text = ""
             movie.isUserInteractionEnabled = false
             movie.text = ""
+            movie.textColor = self.theme[Theme.resultsColor] as? UIColor
         }
         
         self.requestSpeechAutherization()
@@ -184,11 +210,71 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
         self.titleLengthLable.text = "\(Int(self.titleLengthStepperValue.value))"
     }
     
+    func japanTheme() {
+        self.outputLanguage = "ja-JA"
+        
+        self.theme[Theme.startColor] = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        self.theme[Theme.start] = "スタート"
+        self.theme[Theme.stop] = "やめる"
+        self.theme[Theme.backgroundColor] = UIColor.black
+        self.theme[Theme.resultsColor] = UIColor.cyan
+        self.theme[Theme.spokenTextColor] = UIColor.white
+        
+        if let tapToSpeak = self.tapToSpeakButton.title(for: .normal) {
+            if tapToSpeak == "START" {
+                self.tapToSpeakButton.setTitleColor(self.theme[Theme.startColor] as? UIColor, for: .normal)
+                self.tapToSpeakButton.setTitle(self.theme[Theme.start] as? String, for: .normal)
+            } else {
+                self.tapToSpeakButton.setTitle(self.theme[Theme.stop] as? String, for: .normal)
+            }
+        }
+
+        self.titleLengthLable.textColor = self.theme[Theme.spokenTextColor] as? UIColor
+        self.innerView.backgroundColor = self.theme[Theme.backgroundColor] as? UIColor
+        self.spokenTextLabel.textColor = self.theme[Theme.spokenTextColor] as? UIColor
+        view.backgroundColor = self.theme[Theme.backgroundColor] as? UIColor
+        
+        for (index, movie) in self.MovieList.enumerated() {
+            self.yearList[index].textColor = self.theme[Theme.resultsColor] as? UIColor
+            movie.textColor = self.theme[Theme.resultsColor] as? UIColor
+        }
+    }
+    
+    func englishTheme() {
+        self.outputLanguage = "en-AU"
+        
+        self.theme[Theme.startColor] = UIColor.gray
+        self.theme[Theme.start] = "START"
+        self.theme[Theme.stop] = "STOP"
+        self.theme[Theme.backgroundColor] = UIColor.white
+        self.theme[Theme.resultsColor] = #colorLiteral(red: 0.4573462605, green: 0.04310884327, blue: 0, alpha: 1)
+        self.theme[Theme.spokenTextColor] = UIColor.black
+        
+        if let tapToSpeak = self.tapToSpeakButton.title(for: .normal) {
+            if tapToSpeak == "スタート" {
+                self.tapToSpeakButton.setTitleColor(self.theme[Theme.startColor] as? UIColor, for: .normal)
+                self.tapToSpeakButton.setTitle(self.theme[Theme.start] as? String, for: .normal)
+            } else {
+                self.tapToSpeakButton.setTitle(self.theme[Theme.stop] as? String, for: .normal)
+            }
+        }
+        
+        self.titleLengthLable.textColor = self.theme[Theme.spokenTextColor] as? UIColor
+        self.innerView.backgroundColor = self.theme[Theme.backgroundColor] as? UIColor
+        self.spokenTextLabel.textColor = self.theme[Theme.spokenTextColor] as? UIColor
+        view.backgroundColor = self.theme[Theme.backgroundColor] as? UIColor
+        
+        for (index, movie) in self.MovieList.enumerated() {
+            self.yearList[index].textColor = self.theme[Theme.resultsColor] as? UIColor
+            movie.textColor = self.theme[Theme.resultsColor] as? UIColor
+        }
+    }
+    
     @IBAction func langSegmentChanged(_ sender: UISegmentedControl) {
         if let segmentLang = self.languageSegment.titleForSegment(at: sender.selectedSegmentIndex) {
             switch segmentLang {
-            case "JP": return self.outputLanguage = "ja-JA"
-            case "EN": return self.outputLanguage = "en-AU"
+            case "JP": return self.japanTheme()
+            case "EN": return self.englishTheme()
             default: return self.outputLanguage = "en-US"
             }
         }
@@ -200,14 +286,15 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             self.cancelRecording()
             let modifiedMovie = self.spokenTextLabel.text?.split(separator: " ").joined(separator: "%20")
             self.getMovies(at: self.baseUrl, for: modifiedMovie)
+//            self.getMovies(at: self.baseUrl, for: "Batman")
 
             if (self.model.count >= 1) {
                  print("End", self.model[self.model.count - 1].results.count)
             }
            
             isRecording = false
-            tapToSpeakButton.setTitle("START", for: .normal)
-            tapToSpeakButton.setTitleColor(.gray, for: .normal)
+            tapToSpeakButton.setTitle(self.theme[Theme.start] as? String, for: .normal)
+            tapToSpeakButton.setTitleColor(self.theme[Theme.startColor] as? UIColor, for: .normal)
         } else {
             for (index, movie) in self.MovieList.enumerated() {
                 self.yearList[index].text = ""
@@ -217,8 +304,8 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             
             self.recordAndRecognizeSpeech()
             isRecording = true
-            tapToSpeakButton.setTitle("STOP", for: .normal)
-            tapToSpeakButton.setTitleColor(.red, for: .normal)
+            tapToSpeakButton.setTitle(self.theme[Theme.stop] as? String, for: .normal)
+            tapToSpeakButton.setTitleColor(self.theme[Theme.stopColor] as? UIColor, for: .normal)
         }
     }
 }
